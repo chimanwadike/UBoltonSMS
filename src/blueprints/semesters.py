21e7969a07b0cced61885840568e3621d5d57de9
Chima Nwadike <austintappy@gmail.com>
2023-03-22 22:23:22 +0000

    implemented GET, POST, PUT, PATCH, DELETE for semester

--- src/blueprints/semesters.py
+++ src/blueprints/semesters.py
@@ -1,22 +1,24 @@
 from flask import Blueprint, request
-from src.services.semester_service import create_semester, get_semesters, get_semester, delete_semester
+from src.services.semester_service import create_semester, get_semesters, get_semester, delete_semester, edit_semester
 from flasgger import swag_from
 
 semesters = Blueprint("semesters", __name__, url_prefix="/api/v1/semesters")
 
 
 @semesters.get('/')
-@swag_from('../docs/semesters/get.yaml')
+@swag_from('../docs/semesters/get_all.yaml')
 def get_all():
     return get_semesters(request)
 
 
 @semesters.get('/<int:id>')
+@swag_from('../docs/semesters/get.yaml')
 def get_semester(id):
     return get_semester(request, id)
 
 
 @semesters.post('/')
+@swag_from('../docs/semesters/create.yaml')
 def create():
     return create_semester(request)
 
@@ -28,5 +30,6 @@ def delete(id):
 
 @semesters.put('/<int:id>')
 @semesters.patch('/<int:id>')
+@swag_from('../docs/semesters/update.yaml')
 def get(id):
-    return get_semester(request, id)
+    return edit_semester(request, id)
