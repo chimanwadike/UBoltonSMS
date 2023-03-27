0980cc13ddb8c4f0127c5fad56e07be9c8e3221f
l4christ <ademolaemmanuel12@yahoo.com>
2023-03-27 17:42:43 +0100

    Update Documentation

--- src/blueprints/courses.py
+++ src/blueprints/courses.py
@@ -6,31 +6,31 @@ courses = Blueprint("courses", __name__, url_prefix="/api/v1/courses")
 
 
 @courses.get('/')
-# @swag_from('../docs/semesters/get_all.yaml')
+@swag_from('../docs/courses/get_all.yaml')
 def get_all():
     return get_courses(request.args)
 
 
 @courses.get('/<int:id>')
-# @swag_from('../docs/semesters/get.yaml')
+@swag_from('../docs/courses/get.yaml')
 def get_course(id):
     return get_course_by_id(id)
 
 
 @courses.post('/')
-# @swag_from('../docs/semesters/create.yaml')
+@swag_from('../docs/courses/create.yaml')
 def create():
     return create_course(request.get_json())
 
 
 @courses.delete('/<int:id>')
-# @swag_from('../docs/semesters/delete.yaml')
+@swag_from('../docs/courses/delete.yaml')
 def delete(id):
     return delete_course(id)
 
 
 @courses.put('/<int:id>')
 @courses.patch('/<int:id>')
-# @swag_from('../docs/semesters/update.yaml')
+@swag_from('../docs/courses/update.yaml')
 def update_course(id):
     return update_course_by_id(request.get_json(), id)
