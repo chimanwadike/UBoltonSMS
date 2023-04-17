c3c576725d90f5c5e08a3d3a09eddfb63d7913e5
l4christ <ademolaemmanuel12@yahoo.com>
2023-04-17 20:00:48 +0100

    Add seed_user_roles method to the seed_data

--- src/database/seed_data.py
+++ src/database/seed_data.py
@@ -1,5 +1,7 @@
 # Import the seed functions from seed_data.py
-from src.database.database_seeder import seed_users, seed_roles, seed_courses, seed_semesters, seed_venues, seed_lecture_schedules
+from src.database.database_seeder import seed_users, seed_roles, seed_courses, seed_semesters, seed_venues, \
+    seed_lecture_schedules, seed_user_roles
+
 
 def seed_all():
     seed_users()
@@ -8,6 +10,7 @@ def seed_all():
     seed_semesters()
     seed_venues()
     seed_lecture_schedules()
+    seed_user_roles()
 
 # # Call the seed_all() function to run all the seed functions at once
 # seed_all()
