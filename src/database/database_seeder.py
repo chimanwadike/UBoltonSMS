cedb190cb69a779aa500989d98a34396c6feb4f4
l4christ <ademolaemmanuel12@yahoo.com>
2023-04-18 12:31:22 +0100

    Add seed_lecture_sessions and seed_lecture_schedule_student_enrolment

--- src/database/database_seeder.py
+++ src/database/database_seeder.py
@@ -1,6 +1,9 @@
 from datetime import datetime, timedelta
+from random import randint
+
 from faker import Faker
-from src.database.database_context import db, User, Role, Semester, Venue, Course, LectureSchedule
+from src.database.database_context import db, User, Role, Semester, Venue, Course, LectureSchedule, LectureSession, \
+    lecture_schedule_student_enrolment
 from werkzeug.security import generate_password_hash
 
 
@@ -126,6 +129,69 @@ def seed_user_roles():
         user_roles = fake.random_elements(elements=roles, unique=True, length=num_roles)
         user.roles.extend(user_roles)
 
+    db.session.commit()
+
+
+def generate_check_in_code():
+    # Generate random uppercase characters for each segment of the check-in code
+    segment1 = fake.random_uppercase_letter() + fake.random_uppercase_letter()
+    segment2 = fake.random_uppercase_letter() + fake.random_uppercase_letter()
+    segment3 = fake.random_uppercase_letter() + fake.random_uppercase_letter()
+    check_in_code = f"{segment1}-{segment2}-{segment3}"
+    return check_in_code
+
+def seed_lecture_sessions():
+    # Query existing LectureSchedule objects to get their ids
+    lecture_schedules = LectureSchedule.query.all()
+    lecture_schedule_ids = [schedule.id for schedule in lecture_schedules]
+
+    for i in range(10):  # Create 10 lecture sessions
+        lecture_schedule_id = fake.random_element(elements=lecture_schedule_ids)
+        start_time = fake.date_time_this_decade()
+        end_time = start_time + timedelta(hours=fake.random_element(elements=(1, 3)))
+        allow_self_registration = fake.boolean()
+        check_in_code = generate_check_in_code()  # Generate check-in code
+        status = fake.random_element(elements=('active', 'completed'))
+        created_at = datetime.now()
+
+        lecture_session = LectureSession(
+            lecture_schedule_id=lecture_schedule_id,
+            start_time=start_time,
+            end_time=end_time,
+            allow_self_registration=allow_self_registration,
+            check_in_code=check_in_code,
+            status=status,
+            created_at=created_at
+        )
+
+        db.session.add(lecture_session)
+
+    db.session.commit()
+
+
+def seed_lecture_schedule_student_enrolment():
+    # Get all users with role_id of 2 (student)
+    students = User.query.join(User.roles).filter(Role.id == 2).all()
+
+    # Loop through each student and create enrolments
+    for student in students:
+        enrolments = randint(1, 5)  # Randomly generate number of enrolments (1 to 5)
+
+        # Loop through each enrolment
+        for _ in range(enrolments):
+            # Get a random LectureSchedule
+            lecture_schedule = LectureSchedule.query.order_by(db.func.random()).first()
+
+            # Create a new enrolment with lecture_schedule_id and user_id
+            enrolment = lecture_schedule_student_enrolment.insert().values(
+                lecture_schedule_id=lecture_schedule.id,
+                user_id=student.id
+            )
+
+            # Execute the enrolment query
+            db.session.execute(enrolment)
+
+    # Commit the changes
     db.session.commit()
 
 # seed_users()
