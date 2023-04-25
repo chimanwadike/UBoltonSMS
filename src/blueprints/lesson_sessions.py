3463151bf9585f02f60e06c1d5a334dbd098c9e6
Chima Nwadike <austintappy@gmail.com>
2023-04-26 00:14:06 +0100

    completed

--- src/blueprints/lesson_sessions.py
+++ src/blueprints/lesson_sessions.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, request
 from flask_jwt_extended import jwt_required
 
-from src.services.lesson_session_service import get_students_enrolled_in_lesson_session
+from src.services.lesson_session_service import get_students_enrolled_in_lesson_session, register_session_attendance
+from src.utils.auth_decorators import tutor_required
 
 lesson_sessions = Blueprint("lesson_sessions", __name__, url_prefix="/api/v1/lesson_sessions")
 
@@ -10,3 +11,9 @@ lesson_sessions = Blueprint("lesson_sessions", __name__, url_prefix="/api/v1/les
 @jwt_required()
 def students_by_lesson_session(session_id):
     return get_students_enrolled_in_lesson_session(request.args, session_id=session_id)
+
+
+@lesson_sessions.post('/<int:session_id>/attendance')
+@tutor_required()
+def register_lesson_session_attendance(session_id):
+    return register_session_attendance(request, session_id=session_id)
